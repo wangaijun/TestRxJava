fabe91462e6f330198afbe93c6edc9172ee7dadd
wangaijun <hw_wangaijun@163.com>
2018-11-20 17:08:22 +0800

    可运行

---
+++ myapplication2/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication2
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
